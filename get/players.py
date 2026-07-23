6793c22a8b339b8ef418b20b88fe29ac3b5f135b
Kyle <kknoebel@gmail.com>
2026-07-22 18:05:33 -0700

    Adds Players class and get_all_players method

---
+++ get/players.py
@@ -0,0 +1,25 @@
+"""Single location for database interaction the Players class."""
+
+from access.relational import SQLAccess
+
+class Players:
+    def __init__(self):
+        self.sa = SQLAccess()
+        self.sa.__int__()
+        cursor = self.sa.create_connection()
+        self.cursor = cursor
+
+    def get_all_players(self):
+        """Get all unique players in the database."""
+        all_players = []
+        self.cursor.execute("SELECT player_name FROM players.fifa_world_cup_26;")
+        all_entries = self.cursor.fetchall()
+        for entry in all_entries:
+            this_entry = entry[0].strip()
+            all_players.append(this_entry)
+        self.sa.close_connection(self.cursor)
+        all_players_un = set(all_players)
+
+        return all_players_un
+
+
---
+++ tests/test_players.py
@@ -0,0 +1,9 @@
+"""Single location for tests related to the Player class."""
+
+from get.players import Players
+
+def  test_get_all_players():
+    unique_players = 1245
+    pls = Players()
+    all_pls = pls.get_all_players()
+    assert len(all_pls) == unique_players
