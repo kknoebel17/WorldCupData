54a8be01cbca09d7adecc7a495ca4262b8962cdd
Kyle <kknoebel@gmail.com>
2026-07-23 10:30:45 -0700

    Refactors Players class
    Previous dataset found to be bogus, so adding query for new dataset. Also returning a dataframe of first 7 fields for display in streamlit.

--- get/players.py
+++ get/players.py
@@ -1,6 +1,9 @@
 """Single location for database interaction the Players class."""
 
+import pandas as pd
+
 from access.relational import SQLAccess
+from constants import PLAYER_COLS
 
 class Players:
     def __init__(self):
@@ -14,12 +17,18 @@ class Players:
         all_players = []
         self.cursor.execute("SELECT * FROM worldcup26.players;")
         all_entries = self.cursor.fetchall()
-        for entry in all_entries:
-            this_entry = entry[0].strip()
-            all_players.append(this_entry)
         self.sa.close_connection(self.cursor)
-        all_players_un = set(all_players)
 
-        return all_players_un
+        for entry in all_entries:
+            this_entry = entry[:7]
+            all_players.append(this_entry)
+        player_cols = list(PLAYER_COLS.values())
+        cols_to_use = player_cols[:len(all_players[0])]
+        df_all_players = pd.DataFrame(
+            data=all_players,
+            columns=cols_to_use,
+        )
+
+        return df_all_players
 
 
--- tests/test_players.py
+++ tests/test_players.py
@@ -1,9 +1,11 @@
 """Single location for tests related to the Player class."""
 
+from constants import PLAYER_COLS
 from get.players import Players
 
 def  test_get_all_players():
     unique_players = 1248
     pls = Players()
     all_pls = pls.get_all_players()
+    assert all_pls.columns.tolist() == list(PLAYER_COLS.values())[:7]
     assert len(all_pls) == unique_players
